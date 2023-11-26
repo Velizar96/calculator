94719296994acd3fb7baf4fe67a8f9accf2af50b
Velizar96 <velizar.vesovicc@gmail.com>
2023-11-26 15:21:45 +0100

    'done'

--- script.js
+++ script.js
@@ -1,100 +1,127 @@
 "use strict";
 let firstNumber = 0;
 let secondNumber = 0;
-let displayValue = 0;
-let choosenOp;
-let result;
+let currentOperation = null;
+let shouldResetDisplay = false;
 
-const numbers = document.querySelector(".numbers");
-const divid = document.querySelector(".divide");
-const multipl = document.querySelector(".multiply");
-const minus = document.querySelector(".minus");
-const sum = document.querySelector(".plus");
-const operator = document.querySelectorAll(".op");
-const buttons = document.querySelectorAll(".btn");
-const display = document.querySelector(".results");
-const equalOp = document.querySelector(".equal");
-const clearBtn = document.querySelector(".clear");
-const deleteBtn = document.querySelector(".delete");
+const numbers = document.querySelectorAll("[data-number]");
+const operators = document.querySelectorAll("[data-operator]");
+const dotButton = document.querySelector(".dot");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
+const display = document.querySelector(".display");
+const upperDisplay = document.querySelector(".upper-display");
 
-function add(num1, num2) {
-  return num1 + num2;
-}
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-function divide(num1, num2) {
-  return num1 / num2;
-}
+window.addEventListener("keydown", handleKeyboardInput);
+equalButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clearDisplay);
+deleteButton.addEventListener("click", deleteNumber);
+dotButton.addEventListener("click", appendPoint);
 
-function operate(op, firstNumber, secondNumber) {
-  if (op === "×") return multiply(+firstNumber, +secondNumber);
-  if (op === "+") return add(+firstNumber, +secondNumber);
-  if (op === "-") return subtract(+firstNumber, +secondNumber);
-  if (op === "÷") return divide(+firstNumber, +secondNumber);
+numbers.forEach((number) =>
+  number.addEventListener("click", () => appendNumber(number.textContent))
+);
+
+operators.forEach((operator) =>
+  operator.addEventListener("click", () => setOperation(operator.textContent))
+);
+
+function roundValue(number) {
+  return Math.floor(number * 1000) / 1000;
 }
 
-function displayNum() {
-  buttons.forEach((btn) =>
-    btn.addEventListener("click", function (e) {
-      displayValue = e.target.innerText;
-      display.innerText = display.innerText.replace(/^0+/, "");
-      display.innerText = display.innerText + displayValue;
-    })
-  );
+function add(a, b) {
+  return a + b;
 }
-displayNum();
-
-function choosingOperator() {
-  operator.forEach((oper) =>
-    oper.addEventListener("click", function (e) {
-      choosenOp = e.target.innerText;
-      firstNumber = +display.innerText;
-      if (numbers.innerText && display.innerText) {
-        firstNumber = numbers.innerText.charAt(0);
-        console.log(firstNumber);
-        secondNumber = display.innerText.slice(1);
-        display.innerText = display.innerText.toString().slice(1);
-        console.log(secondNumber);
-        display.innerText = operate(choosenOp, +firstNumber, +secondNumber);
-      }
-      display.innerText = 0;
-      numbers.innerText = `${firstNumber}${choosenOp}`;
-    })
-  );
+function subtract(a, b) {
+  return a - b;
 }
-choosingOperator();
-
-function equal() {
-  equalOp.addEventListener("click", function (e) {
-    // display.innerText = 0;
-    secondNumber = +display.innerText;
-    console.log(`Second number: ${secondNumber}`);
-    display.innerText = operate(choosenOp, +firstNumber, +secondNumber);
-  });
+function multiply(a, b) {
+  return a * b;
 }
-equal();
-
-function clear() {
-  clearBtn.addEventListener("click", function () {
-    display.innerText = 0;
-    firstNumber = 0;
-    secondNumber = 0;
-  });
+function divide(a, b) {
+  return a / b;
 }
-clear();
 
-function deleteNum() {
-  deleteBtn.addEventListener("click", function () {
-    if (firstNumber && secondNumber) {
-      return;
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return subtract(a, b);
+  if (operator === "×") return multiply(a, b);
+  if (operator === "÷") {
+    if (b === 0) {
+      return null;
+    } else {
+      return divide(a, b);
     }
-    let dis = display.innerText;
-    let cutDis = dis.slice(0, -1);
-    display.innerText = +cutDis;
-  });
+  }
 }
-deleteNum();
+
+function convertOperator(operator) {
+  if (operator === "+") return "+";
+  if (operator === "-") return "−";
+  if (operator === "*") return "×";
+  if (operator === "/") return "÷";
+}
+
+function resetScreen() {
+  display.textContent = "";
+  shouldResetDisplay = false;
+}
+
+function appendNumber(number) {
+  if (display.textContent === "0" || shouldResetDisplay) resetScreen();
+  display.textContent += number;
+}
+
+function deleteNumber() {
+  display.textContent = display.textContent.toString().slice(0, -1);
+}
+
+function clearDisplay() {
+  display.textContent = 0;
+  upperDisplay.textContent = "";
+  firstNumber = "";
+  secondNumber = "";
+  currentOperation = null;
+}
+
+function appendPoint() {
+  if (shouldResetDisplay) resetScreen();
+  if (display.textContent === "") display.textContent = "0";
+  if (display.textContent.includes(".")) return (display.textContent += ".");
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstNumber = display.textContent;
+  currentOperation = operator;
+  upperDisplay.textContent = `${firstNumber} ${currentOperation}`;
+  shouldResetDisplay = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetDisplay) return;
+  if (currentOperation === "÷" && display.textContent === "0") {
+    alert("You can't divide by 0!");
+    return;
+  }
+  secondNumber = display.textContent;
+  display.textContent = roundValue(
+    operate(currentOperation, firstNumber, secondNumber)
+  );
+  upperDisplay.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =`;
+  currentOperation = null;
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === "Escape") clearDisplay();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === ".") appendPoint();
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+    setOperation(convertOperator(e.key));
+}
